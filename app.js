3c48fd3d9d49f5f18ed2bf452d074b8f701d6c45
r1qk <riquelmesmatamoh@gmail.com>
2025-06-06 15:17:45 -0300

    Teste de github

---
+++ app.js
@@ -0,0 +1,62 @@
+alert('Atividade da aula 2')
+//Exercício 1
+// let day = prompt('Que dia é hoje?');
+// if (day == 'Sábado' || day == 'Domingo') {
+//     alert('Bom final de semana!')
+// } else {
+//     alert('Boa semana!')
+// }
+
+//ou
+
+// let day = prompt('Que dia é hoje?');
+// if (day == 'Sábado') {
+//     alert('Bom final de semana!')
+// } else {
+//     if (day == 'Domingo') {
+//         alert('Bom final de semana!')
+//     } else {
+//         alert('Boa semana!')
+//     }
+// }
+
+// =========================================
+
+//Exercício 2
+// let numero = prompt('Digite um número');
+// if (numero > 0) {
+//     alert('Seu número é positivo');
+// } else {
+//     if (numero < 0) {
+//         alert('Seu número é negativo');
+//     }
+// }
+// if (numero = 0) {
+//     alert('Seu número é 0');
+// }
+// =========================================
+
+//Exercício 3
+// let pont = prompt('Digite a pontuação do seu jogo.')
+// if (pont >= 100) {
+//     console.log('Parabéns! Você venceu!')
+// } else {
+//     console.log('Tente novamente para ganhar.')
+// }
+// =========================================
+
+//Exercício 4 (OBS: só as duas primeiras linhas já terminam o exercício)
+// let saldo = prompt('Qual o seu saldo?');
+// alert(`O seu saldo é de ${saldo}`);
+// let add = prompt('Digite o quanto você quer adicionar.');
+// alert(`Você irá adicionar ${add}`);
+// let novoSaldo = Number(saldo) + Number(add)
+// alert(`Se você somar ${saldo} com ${add}, ficará com ${novoSaldo}`);
+// =========================================
+
+//Exercício 5
+// let myName = prompt('Qual o seu nome?')
+// alert(`Opa, eae ${myName}`);
+
+
+
